6d6147b7fe243c5dad0406dd54815b0a49dd8f33
DonMizzy <ejeanobionyeka@gmail.com>
2025-03-25 20:18:33 +0100

    edited the worked seo

--- RMHSA-project/pages/blogs/[id].js
+++ RMHSA-project/pages/blogs/[id].js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Head from "next/head";
 import { useRouter } from "next/router"; // If using Next.js
 import Link from "next/link";
 import Loading from "@/components/loading";
