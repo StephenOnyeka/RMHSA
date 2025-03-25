06b9e67b4e3185c591fc8b05634267e8ec167e9f
DonMizzy <ejeanobionyeka@gmail.com>
2025-03-25 01:06:15 +0100

    worked on the benefacio page

--- RMHSA-project/pages/give-to-RMHS.js
+++ RMHSA-project/pages/give-to-RMHS.js
@@ -19,25 +19,6 @@ function Donate() {
         <Topfile />
         <ScrollDiv />
         <div className=" h-full font-Poppins overflow-hidden">
-          {/* <div className="bg-[url('/Edited/1732230029150.jpg')] bg-cover bg-center">
-            <div
-              id="Donor_container-1"
-              className="bg-black/70 h-screen max-md:h-screen/1.5 px-8 pt-8 max-sm:px-4 w-full font-Poppins text-white"
-            >
-              <div className="">
-                <Navbar />
-              </div>
-
-              <div className="absolute z-1 bottom-64 lg:bottom-8 max-sm:bottom-80 text-white">
-                <p className="text-xl pb-3 max-sm:text-lg max-sm:pb-1">
-                  Become a donor
-                </p>
-                <p className="text-5xl font-bold max-sm:text-3xl">
-                  Give To Rosa Mystica
-                </p>
-              </div>
-            </div>
-          </div> */}
           <div className="bg-[url('/Edited/1732230029150.jpg')] bg-cover bg-center">
             <div
               id="Donor_container-1"
@@ -90,7 +71,7 @@ function Donate() {
                       About Rosa Mystica Fund
                     </p>
                     <br />
-                    <p className=" text-gray-500 leading-7 max-sm:text-sm max-sm:leading-6">
+                    {/* <p className=" text-gray-500 leading-7 max-sm:text-sm max-sm:leading-6">
                       We continue to adapt to new ways of teaching, new
                       disciplines of study and new ways of learning. Your gift
                       enriches the experience of all students by supporting
@@ -105,35 +86,42 @@ function Donate() {
                       seamlessly into the workforce upon your graduation, but we
                       also make sure that you will have a good sense of social
                       justice so that you make the transition responsibly.
+                    </p> */}
+                    <p className=" text-gray-500 leading-7 max-sm:text-sm max-sm:leading-6">
+                      Rosa Mystica High School are committed to developing
+                      academically strong, socially responsible leaders. To
+                      maintain our high standards and expand our reach, we need
+                      your support. Your investment empowers us to attract top
+                      faculty, develop innovative programs, and ensure
+                      accessibility for deserving students.
+                    </p>
+                    <br />
+                    <p className=" text-gray-500 leading-7 max-sm:text-sm max-sm:leading-6">
+                      By contributing to the Rosa Mystica Fund, you're not just
+                      donating; you're shaping the future. Your generosity
+                      directly impacts our students and the communities they'll
+                      serve. Join us in our mission to empower the next
+                      generation.
                     </p>
                     <br />
                     {/* <div className="h-64 w-full bg-[url('/RMHS/IMG_20241015_115530~2.jpg')] bg-cover bg-no-repeat bg-center"></div> */}
-                    <Image
+                    {/* <Image
                       src="/Edited/WhatsApp Image 2024-12-07 at 16.00.26_6fee0c7a.jpg"
                       className="pt-8 w-full h-full"
                       width={500}
                       height={250}
                       layout="responsive"
                       alt=""
+                    /> */}
+                    <Image
+                      // src="/Edited/WhatsApp Image 2024-12-07 at 16.00.26_6fee0c7a.jpg"
+                      src="/RMHS/IMG_20241015_140402.jpg"
+                      className="pt-8 w-full h-full"
+                      width={500}
+                      height={250}
+                      layout="responsive"
+                      alt=""
                     />
-                    {/* <Image
-                      src="/RMHS/IMG_20241015_115530~2.jpg"
-                      className="pt-8"
-                      width={500}
-                      height={250}
-                      layout="responsive"
-                      alt=""
-                    /> */}
-                    {/* <Image
-                      src="/RMHS/IMG_20241015_115530~2.jpg"
-                      className="pt-8"
-                      // width={"100%"}
-                      // height={"100%"}
-                      width={500}
-                      height={250}
-                      layout="responsive"
-                      alt=""
-                    /> */}
                   </div>
 
                   <div id="" className="py-16 ">
@@ -276,62 +264,9 @@ function Donate() {
                       </div>
                       <div className="pb-6">
                         <p>+1-2345-6789-10</p>
-                        <p>sample@hiskool.edu</p>
+                        <p>rosamysticahsa@gmail.com</p>
                       </div>
                       <p className="pb-6">Mon-Fri 9:00A.M.-5:00P.M.</p>
-                      {/* <p className="font-bold text-xl py-4">Social Info</p>
-                    <div className="flex gap-4">
-                      <svg
-                        stroke="currentColor"
-                        fill="currentColor"
-                        stroke-width="0"
-                        viewBox="0 0 320 512"
-                        className="text-primary w-5 h-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
-                      </svg>
-                      <svg
-                        stroke="currentColor"
-                        fill="currentColor"
-                        stroke-width="0"
-                        viewBox="0 0 448 512"
-                        className="text-primary w-5 h-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
-                      </svg>
-                      <svg
-                        stroke="currentColor"
-                        fill="currentColor"
-                        stroke-width="0"
-                        viewBox="0 0 448 512"
-                        className="text-primary w-5 h-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M424.7 299.8c2.9-14 4.7-28.9 4.7-43.8 0-113.5-91.9-205.3-205.3-205.3-14.9 0-29.7 1.7-43.8 4.7C161.3 40.7 137.7 32 112 32 50.2 32 0 82.2 0 144c0 25.7 8.7 49.3 23.3 68.2-2.9 14-4.7 28.9-4.7 43.8 0 113.5 91.9 205.3 205.3 205.3 14.9 0 29.7-1.7 43.8-4.7 19 14.6 42.6 23.3 68.2 23.3 61.8 0 112-50.2 112-112 .1-25.6-8.6-49.2-23.2-68.1zm-194.6 91.5c-65.6 0-120.5-29.2-120.5-65 0-16 9-30.6 29.5-30.6 31.2 0 34.1 44.9 88.1 44.9 25.7 0 42.3-11.4 42.3-26.3 0-18.7-16-21.6-42-28-62.5-15.4-117.8-22-117.8-87.2 0-59.2 58.6-81.1 109.1-81.1 55.1 0 110.8 21.9 110.8 55.4 0 16.9-11.4 31.8-30.3 31.8-28.3 0-29.2-33.5-75-33.5-25.7 0-42 7-42 22.5 0 19.8 20.8 21.8 69.1 33 41.4 9.3 90.7 26.8 90.7 77.6 0 59.1-57.1 86.5-112 86.5z"></path>
-                      </svg>
-                      <svg
-                        stroke="currentColor"
-                        fill="currentColor"
-                        stroke-width="0"
-                        viewBox="0 0 512 512"
-                        className="text-primary w-5 h-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
-                      </svg>
-                      <svg
-                        stroke="currentColor"
-                        fill="currentColor"
-                        stroke-width="0"
-                        viewBox="0 0 448 512"
-                        className="text-primary w-5 h-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path>
-                      </svg>
-                    </div> */}
                     </div>
                   </>
 
@@ -370,67 +305,11 @@ function Donate() {
                       </div>
                       <div className="pb-6">
                         <p>+1-2345-6789-10</p>
-                        <p>sample@hiskool.edu</p>
+                        <p>rosamysticahsa@gmail.com</p>
                       </div>
                       <p className="pb-6 text-secondary">
                         Mon-Fri 9:00A.M.-5:00P.M.
                       </p>
-                      {/* <p className="font-bold text-xl py-4">Social Info</p>
-                    <div className="flex gap-4 text-secondary pb-12">
-                      <svg
-                        stroke="currentColor"
-                        fill="currentColor"
-                        stroke-width="0"
-                        viewBox="0 0 320 512"
-                        className=" w-5 h-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
-                      </svg>
-                      <svg
-                        stroke="currentColor"
-                        fill="currentColor"
-                        stroke-width="0"
-                        viewBox="0 0 448 512"
-                        className=" w-5 h-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
-                      </svg>
-                      <svg
-                        stroke="currentColor"
-                        fill="currentColor"
-                        stroke-width="0"
-                        viewBox="0 0 448 512"
-                        className=" w-5 h-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M424.7 299.8c2.9-14 4.7-28.9 4.7-43.8 0-113.5-91.9-205.3-205.3-205.3-14.9 0-29.7 1.7-43.8 4.7C161.3 40.7 137.7 32 112 32 50.2 32 0 82.2 0 144c0 25.7 8.7 49.3 23.3 68.2-2.9 14-4.7 28.9-4.7 43.8 0 113.5 91.9 205.3 205.3 205.3 14.9 0 29.7-1.7 43.8-4.7 19 14.6 42.6 23.3 68.2 23.3 61.8 0 112-50.2 112-112 .1-25.6-8.6-49.2-23.2-68.1zm-194.6 91.5c-65.6 0-120.5-29.2-120.5-65 0-16 9-30.6 29.5-30.6 31.2 0 34.1 44.9 88.1 44.9 25.7 0 42.3-11.4 42.3-26.3 0-18.7-16-21.6-42-28-62.5-15.4-117.8-22-117.8-87.2 0-59.2 58.6-81.1 109.1-81.1 55.1 0 110.8 21.9 110.8 55.4 0 16.9-11.4 31.8-30.3 31.8-28.3 0-29.2-33.5-75-33.5-25.7 0-42 7-42 22.5 0 19.8 20.8 21.8 69.1 33 41.4 9.3 90.7 26.8 90.7 77.6 0 59.1-57.1 86.5-112 86.5z"></path>
-                      </svg>
-                      <svg
-                        stroke="currentColor"
-                        fill="currentColor"
-                        stroke-width="0"
-                        viewBox="0 0 512 512"
-                        className=" w-5 h-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
-                      </svg>
-                      <svg
-                        stroke="currentColor"
-                        fill="currentColor"
-                        stroke-width="0"
-                        viewBox="0 0 448 512"
-                        className=" w-5 h-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path>
-                      </svg>
-                    </div> */}
-                      {/* <button className="bg-primary p-4 px-6 font-bold">
-                      Student Resources
-                    </button> */}
                     </div>
                   </>
                 </div>
@@ -442,14 +321,6 @@ function Donate() {
             {/* <div className="text-white h-screen/2 max-sm:absolute z-1 mb-32"> */}
             <div className="bg-contingentColor flex gap-10 items-center justify-center px-6 pt-24 w-full py-28 max-sm:flex-wrap max-sm:py-16">
               <div className="w-2/4 px-8 max-sm:w-full max-sm:px-4">
-                {/* <Image
-                  src="/RMHS/IMG_20241015_114703.jpg"
-                  alt=""
-                  width={"100%"}
-                  height={"100%"}
-                  layout="responsive"
-                  className="w-full -mt-40 max-sm:-mt-32"
-                /> */}
                 <Image
                   src="/Edited/1732230029137.jpg"
                   alt=""
@@ -458,14 +329,6 @@ function Donate() {
                   layout="responsive"
                   className="w-full -mt-40 max-sm:-mt-32"
                 />
-                {/* <Image
-                  src="/RMHS/IMG_20241015_114703.jpg"
-                  alt=""
-                  width={500}
-                  height={200}
-                  layout="responsive"
-                  className="w-full -mt-40 max-sm:-mt-32"
-                /> */}
               </div>
               <div className="w-2/4 flex flex-col gap-12 max-sm:w-full max-sm:gap-6">
                 <p className="text-2xl leading-10 max-sm:text-xl max-sm:leading-8">
@@ -474,7 +337,7 @@ function Donate() {
                   world.
                 </p>
                 <p>- School Principal</p>
-                <div className="">
+                {/* <div className="">
                   <button className="bg-primary flex gap-2 text-sm font-semibold p-4 px-6 max-sm:text-xs">
                     Become A Donor
                     <svg
@@ -496,7 +359,7 @@ function Donate() {
                       />
                     </svg>
                   </button>
-                </div>
+                </div> */}
               </div>
               {/* <Footer /> */}
             </div>
